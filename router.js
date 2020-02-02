92dab96068b1f63142e9221a92cb51be66e09aa3
Robert Seidler <robert.seidler1@googlemail.com>
2020-02-02 07:26:18 +0100

    streamlined server error handler; removed unused blob function;

--- router.js
+++ router.js
@@ -14,18 +14,17 @@ const {
   insertRegStmt, 
   selectRegistrationColumns,
   selectRegistrations,
-  selectRegistrationsForApplication,
   insertExcStmt,
   selectReportDataTextStmt,
   selectExceptionColumns,
   seletExceptions,
 } = require('./database.js');
 
-function handleGetError(res, next, error){
+function handleError(res, next, statusCode, error){
   console.error(error);
   res.locals.template = errorViewPath;
   res.locals.content = {
-    status: 400,
+    status: statusCode,
     error: error.stack,
     template: 'error/error'
   };
@@ -76,26 +75,6 @@ function sendResultPage(pageName, sidebarTemplate, req, res, next){
     })
 }
 
-function handlePostError(res, next, error){
-  console.error(error);
-  res
-    .status(400)
-    .json({
-      error: error.name,
-      message: error.message,
-    });
-}
-
-function handleGetNotFoundError(res, next, error){
-  console.error(error);
-  res
-    .status(404)
-    .json({
-      error: error.name,
-      message: error.message,
-    })
-}
-
 function insertRegistrationRoute(db, req, res, next){
   let now = (new Date()).toISOString();
   console.log({...req.body, ...{time: now}});
@@ -108,42 +87,23 @@ function insertRegistrationRoute(db, req, res, next){
     .then(() => {
       res.sendStatus(200);
     })
-    .catch(handlePostError.bind(this, res, next));
+    .catch(handleError.bind(this, res, next, 400));
 }
 
 function getReportAttachment(db, req, res, next){
   let reportID = req.params.reportID;
   db.all(selectReportDataTextStmt, [reportID])
-    .catch(handleGetNotFoundError.bind(this, res, next))
-    .then(dataRows => (console.log(dataRows[0].DataText), res.send(JSON.parse(dataRows[0].DataText))))
-    // .catch(err => res.send(err))
-}
-
-//TODO: could be refactored into rhtml renderer
-function createDataBlobHtml(data, appname){
-  let dataString = JSON.stringify({result: data}, null, 2);
-  let dataSignature = Buffer.from(dataString).toString('base64').slice(0, 16);
-  
-  return `
-    <a class="${dataSignature} app-${appname}" target="_blank"></a>
-    <script>
-      document.addEventListener('DOMContentLoaded', function(){
-        let anchorElements = document.querySelectorAll('a.${dataSignature}.app-${appname}');
-        let blob = new Blob([\`${dataString}\`], {type: 'application/json'});
-        let blobUrl = URL.createObjectURL(blob);
-        anchorElements.forEach(anchorElement => {
-          anchorElement.setAttribute('href', blobUrl);
-          anchorElement.innerText = 'link';
-        });
-      });
-    </script>
-  `;
+    .catch(handleError.bind(this, res, next, 404))
+    .then(dataRows => (
+      console.log(dataRows[0].DataText), 
+      res.send(JSON.parse(dataRows[0].DataText))
+    ))
+    .catch(handleError.bind(this, res, next, 500))
 }
 
 function insertExceptionRoute(db, req, res, next){
   let now = (new Date()).toISOString();
-  // console.log({...req.body, ...{time: now}});
-  console.log(JSON.stringify({data: req.body.data}))
+  console.log({...req.body, ...{time: now}});
   db.run(insertExcStmt, [
     req.body.application, 
     req.body.severity, 
@@ -156,7 +116,7 @@ function insertExceptionRoute(db, req, res, next){
     .then(() => {
       res.sendStatus(200);
     })
-    .catch(handlePostError.bind(this, res, next))
+    .catch(handleError.bind(this, res, next, 400))
 }
 
 function prepareDisplayValues([columnNames, dataRows]){
@@ -182,8 +142,6 @@ function displayColumns(template, res, next, [columnNames, dataRows]) {
   next();
 }
 
-
-
 function showRegistrations(db, req, res, next){
   let userToken = req.query.user || '%';
   let application = req.query.app || '%';
@@ -192,7 +150,7 @@ function showRegistrations(db, req, res, next){
     db.all(selectRegistrations, [userToken, application])
   ])
     .then(displayColumns.bind(this, showRegistrationsTemplate, res, next))
-    .catch(handleGetError.bind(this, res, next));
+    .catch(handleError.bind(this, res, next, 400));
 }
 
 function showExceptions(db, req, res, next){
@@ -203,7 +161,7 @@ function showExceptions(db, req, res, next){
     db.all(seletExceptions, [userToken, application])
   ])
     .then(displayColumns.bind(this, showExeptionsTemaplate, res, next))
-    .catch(handleGetError.bind(this, res, next));
+    .catch(handleError.bind(this, res, next, 400));
 }
 
 module.exports = {
@@ -213,5 +171,4 @@ module.exports = {
   showExceptions,
   sendResultPage,
   getReportAttachment,
-
 };
